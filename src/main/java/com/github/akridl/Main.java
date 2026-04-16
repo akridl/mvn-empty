19ebfbb982bfd56559677be41b3d17b165536510
Adam Krídl <akridl@redhat.com>
2026-04-16 14:10:15 +0200

    Make the project more minimalistic

--- src/main/java/com/github/akridl/Main.java
+++ src/main/java/com/github/akridl/Main.java
@@ -1,13 +1,8 @@
 package com.github.akridl;
 
-import org.apache.commons.lang3.math.NumberUtils;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {3, -1, 4, 1, 42, 5, 9};
-        int maxNumber = NumberUtils.max(array);
-
-        System.out.println("Max number is: " + maxNumber);
+        System.out.println("Hello there!");
     }
 }
